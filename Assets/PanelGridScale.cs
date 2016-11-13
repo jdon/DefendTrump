85f07149feafaad00ca5ba859427403ea41157df
Alexander Kalinin <kalininator@gmail.com>
2016-11-13 02:11:53 +0000

    ayy lmao roflcopter
    choo choo

---
+++ Assets/PanelGridScale.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PanelGridScale : MonoBehaviour {
+
+    GridLayoutGroup gridLayoutGroup;
+    RectTransform rect;
+	// Use this for initialization
+	void Start () {
+        gridLayoutGroup = GetComponent<GridLayoutGroup>();
+        rect = GetComponent<RectTransform>();
+
+        //gridLayoutGroup.cellSize = new Vector2(rect.rect.height, rect.rect.height);
+        //rect.rect.Set(rect.rect.x,rect.rect.y,rect.rect.width,gridLayoutGroup.cellSize.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
